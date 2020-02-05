b17ce9953d0d835f27188e86dabfb43cbcab8304
simplyjuanc <37302562+simplyjuanc@users.noreply.github.com>
2020-02-05 09:02:11 +0000

    Small adjustments to linter-syntax .

--- bot.py
+++ bot.py
@@ -17,16 +17,16 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 ###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
 try:
-        MESSAGE = os.environ.get('WELCOME_MESSAGE')
-        TOKEN = os.environ.get('SLACK_TOKEN')
-        CHANNEL_TOKEN = os.environ.get('CHANNEL_TOKEN')
-        UNFURL = os.environ.get('UNFURL_LINKS')
+    MESSAGE = os.environ.get('WELCOME_MESSAGE')
+    TOKEN = os.environ.get('SLACK_TOKEN')
+    CHANNEL_TOKEN = os.environ.get('CHANNEL_TOKEN')
+    UNFURL = os.environ.get('UNFURL_LINKS')
 	RESPONSE_CHANNEL = os.environ.get('RESPONSE_CHANNEL')
-        DEBUG_CHANNEL_ID = os.environ.get('DEBUG_CHANNEL_ID', False)
+    DEBUG_CHANNEL_ID = os.environ.get('DEBUG_CHANNEL_ID', False)
 except:
-        MESSAGE = 'Manually set the Message if youre not running through heroku or have not set vars in ENV'
-        TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
-        UNFURL = 'FALSE'
+    MESSAGE = 'Manually set the Message if youre not running through heroku or have not set vars in ENV'
+    TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
+    UNFURL = 'FALSE'
 ###############################################################
 
 def is_team_join(msg):
@@ -35,7 +35,7 @@ def is_team_join(msg):
 def is_debug_channel_join(msg):
     return msg['type'] == "member_joined_channel" and msg['channel'] == DEBUG_CHANNEL_ID and msg['channel_type'] == 'C'
 
-def is_direct_message(msg): 
+def is_direct_message(msg):
     print msg
     is_bot = False
     if 'bot_id' in msg:
@@ -85,14 +85,14 @@ def parse_join(message):
 
         # Need to get the display name from the user_id
         real_name = get_display_name(user_id)
-				
+
         #logging.DEBUG('SENDING MESSAGE: '+user_message+' TO USER '+real_name)
         # Need to send a message to a channel
         requests.get("https://slack.com/api/chat.postMessage?token="+CHANNEL_TOKEN+"&channel="+RESPONSE_CHANNEL+"&text="+user_message+"&as_user=false&username="+real_name)
 
 #Connects to Slacks and initiates socket handshake
 def start_rtm():
-    
+
     r = requests.get("https://slack.com/api/rtm.start?token="+TOKEN, verify=False)
     r = r.json()
     logging.info(r)
